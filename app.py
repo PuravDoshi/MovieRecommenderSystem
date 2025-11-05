3faff0353e26f56bee871ecc0a4ed0ddfef30bcc
Purav Nirav Doshi <pndoshi07@gmail.com>
2025-11-05 19:27:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,55 @@
-import streamlit as st 
+import streamlit as st
 import pickle
 import pandas as pd
+import requests
 
-movie_dict=pickle.load(open(r'/Users/puravdoshi/Downloads/MovieRecommenderSystem/movie_list.pkl','rb'))
-similarity=pickle.load(open(r'/Users/puravdoshi/Downloads/MovieRecommenderSystem/similarity.pkl','rb'))
-movies=pd.DataFrame(movie_dict)
+st.set_page_config(page_title="🎬 Movie Recommender", page_icon="🎥", layout="wide")
+
+movie_dict = pickle.load(open(r'/Users/puravdoshi/Downloads/MovieRecommenderSystem/movie_list.pkl', 'rb'))
+similarity = pickle.load(open(r'/Users/puravdoshi/Downloads/MovieRecommenderSystem/similarity.pkl', 'rb'))
+movies = pd.DataFrame(movie_dict)
+
+def fetch_poster(movie_title):
+    api_key = "" 
+    try:
+        url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={movie_title}"
+        response = requests.get(url)
+        data = response.json()
+        if data.get('results'):
+            poster_path = data['results'][0].get('poster_path')
+            if poster_path:
+                return f"https://image.tmdb.org/t/p/w500{poster_path}"
+        return None
+    except:
+        return None
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])[1:6]
-    recommended_movie_names = []
-    for i in distances:
-        recommended_movie_names.append(movies.iloc[i[0]].title)
-    return recommended_movie_names
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])[1:4]  # top 3
+    recommended_movies = [movies.iloc[i[0]].title for i in distances]
+    return recommended_movies
 
-st.title('Movie Recommender System')
+st.markdown("<h1 style='text-align: center; color: #ff4b4b;'>🎬 Movie Recommender System</h1>", unsafe_allow_html=True)
+st.write("### Discover movies similar to your favorites!")
 
-option=st.selectbox("What movie do you want to watch?",movies['title'].values)
+option = st.selectbox("🎞️ Choose a movie you like:", movies['title'].values)
 
-if(st.button("Recommend")):
-    recommendations=recommend(option)
-    for i in recommendations:
-        st.write(i)
+if st.button("🔍 Get Recommendations"):
+    recommendations = recommend(option)
+    st.markdown("---")
+    st.subheader("✨ Recommended Movies for You:")
+
+    cols = st.columns(3)
+    for idx, movie in enumerate(recommendations):
+        poster_url = fetch_poster(movie)
+        with cols[idx]:
+            if poster_url:
+                st.image(poster_url, width=200)  
+            st.markdown(f"<div style='text-align:center; font-weight:600; color:#333;'>🎥 {movie}</div>", unsafe_allow_html=True)
+
+st.markdown("""
+    <hr>
+    <div style='text-align: center; color: gray;'>
+        Developed with ❤️ using <b>Streamlit</b> | Purav Doshi
+    </div>
+""", unsafe_allow_html=True)
